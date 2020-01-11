fe700993e82a71312f8c5455617b856c7b400218
Vincent Wochnik <v.wochnik@gmail.com>
2020-01-11 16:48:25 +0100

    fix

--- src/components/water-box.ts
+++ src/components/water-box.ts
@@ -1,38 +1,38 @@
 import { html, LitElement, customElement, property } from 'lit-element';
-import renderer, { OPTIONS, RenderingOptions } from '../renderer';
+import renderer, { RenderingOptions } from '../renderer';
 
 @customElement('water-box')
 export default class WaterBox extends LitElement {
 
   @property({ type: Number })
-  width = 320;
+  width: number = 320;
 
   @property({ type: Number })
-  height = 320;
+  height: number = 320;
 
   @property({ type: Number })
-  value = 50;
+  value: number = 50;
 
   @property({ type: String, attribute: 'border-color' })
-  borderColor = 'rgba(127,127,127,0.8)';
+  borderColor: string = 'rgba(127,127,127,0.8)';
 
   @property({ type: Number, attribute: 'border-width' })
-  borderWidth = 1;
+  borderWidth: number = 1;
 
   @property({ type: String, attribute: 'fill-color' })
-  fillColor = 'rgba(32,158,224,0.8)';
+  fillColor: string = 'rgba(32,158,224,0.8)';
 
   @property({ type: String, attribute: 'background-color' })
-  backgroundColor = 'rgba(128,128,128,0.4)';
+  backgroundColor: string = 'rgba(128,128,128,0.4)';
 
   @property({ type: Number })
-  separators = 5;
+  separators: number = 5;
 
   @property({ type: Boolean, attribute: 'draw-top' })
-  drawTop = false;
+  drawTop: boolean = false;
 
   @property({ type: Number })
-  contrast = 20;
+  contrast: number = 20;
 
   render() {
     return html`<canvas width="${this.width}" height="${this.height}"></canvas>`;
@@ -43,7 +43,18 @@ export default class WaterBox extends LitElement {
       const canvas = <HTMLCanvasElement> this.shadowRoot.querySelector("canvas");
       if (canvas !== null) {
         const ctx = <CanvasRenderingContext2D> canvas.getContext("2d");
-        const options = <RenderingOptions> OPTIONS.reduce((obj, option) => { obj[option] = this[option]; return obj; }, {});
+        const options: RenderingOptions = {
+          width: this.width,
+          height: this.height,
+          borderColor: this.borderColor,
+          fillColor: this.fillColor,
+          backgroundColor: this.backgroundColor,
+          borderWidth: this.borderWidth,
+          separators: this.separators,
+          drawTop: this.drawTop,
+          contrast: this.contrast,
+          value: this.value
+        };
         renderer(ctx, options);
       }
     }
